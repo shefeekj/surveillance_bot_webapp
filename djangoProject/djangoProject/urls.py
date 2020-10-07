c0030139dc20fe9747f9611c5614e9774bfc528d
shefeekj <55088049+shefeekj@users.noreply.github.com>
2020-10-07 18:51:44 +0530

    Update urls.py

--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -18,8 +18,18 @@ from django.urls import path
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views, views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+#    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    url(r'^logout/$', auth_views.LoginView.as_view(template_name="registration/logout.html"), name="logout"),
+
     path('', include('surveillance_bot_webapp.urls'))
+#    path('', TemplateView.as_view(template_name='home.html'), name='home'), # new
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
