ffef3556579a7326f2ad0caa41751b084b936a6e
shefeekj <55088049+shefeekj@users.noreply.github.com>
2020-10-07 18:51:02 +0530

    Update views.py

--- djangoProject/surveillance_bot_webapp/views.py
+++ djangoProject/surveillance_bot_webapp/views.py
@@ -1,8 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
 # Create your views here.
 # This function will return and render the home page when url is http://localhost:8000/to_do/.
+
+@login_required(login_url='/login')
 def index_page(request):
     # Get the index template file absolute path.
     # index_file_path = PROJECT_PATH + '/pages/home.html'
     # Return the index file to client.
-    return render(request, 'index.html')
+    print("Reached here")
+    return render(request, 'home.html')
